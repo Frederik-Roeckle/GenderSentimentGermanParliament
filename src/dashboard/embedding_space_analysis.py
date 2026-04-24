493d1dfdc904a2bb5d54b5d767131b0391d03290
Frederik Roeckle <frederik.roeckle@gmx.de>
2026-04-24 14:55:58 +0200

    normalized

--- src/dashboard/embedding_space_analysis.py
+++ src/dashboard/embedding_space_analysis.py
@@ -13,7 +13,7 @@ def load_models():
     return {"2-5": model_2_5, "6-9": model_6_9, "10-14": model_10_14, "15-19": model_15_19}
 
 def word_gender_orientation(word, model):
-    return np.dot(model.wv.get_vector(word), (model.wv.get_mean_vector(["herr", "kollege", "mann"]) - model.wv.get_mean_vector(["frau", "kollegin", "dame"])))
+    return np.dot(model.wv.get_vector(word, norm=True), (model.wv.get_mean_vector(["herr", "kollege", "mann"], post_normalize=True) - model.wv.get_mean_vector(["frau", "kollegin", "dame"], post_normalize=True)))
 
 def word_gender_over_time(word):
     models = load_models()
@@ -23,6 +23,8 @@ def word_gender_over_time(word):
 
 
 st.title("Embedding Space Analysis")
+st.write("value < 1: bias towards female")
+st.write("value > 1: bias towards male")
 
 
 word = st.text_input("Enter word to analyze...")
