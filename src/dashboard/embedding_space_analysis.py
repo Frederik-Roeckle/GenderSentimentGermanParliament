0f2ce94d0a57e683b0bd33228850732243e744b6
Frederik Roeckle <frederik.roeckle@gmx.de>
2026-04-25 10:39:37 +0200

    update dashboard for vis

--- src/dashboard/embedding_space_analysis.py
+++ src/dashboard/embedding_space_analysis.py
@@ -4,6 +4,10 @@ from gensim.models import FastText
 import pandas as pd
 import numpy as np
 
+
+if 'df' not in st.session_state:
+    st.session_state['df'] = pd.DataFrame(data={"word": [], "P2-5": [], "P6-9": [], "P10-14": [], "P15-19": [],})
+    st.session_state['df'] = st.session_state['df'].set_index("word")
 @st.cache_resource
 def load_models():
     model_2_5 = FastText.load("./models/parliament_fasttext_2_5.model")
@@ -17,12 +21,18 @@ def word_gender_orientation(word, model):
 
 def word_gender_over_time(word):
     models = load_models()
+    if word in st.session_state['df'].index:
+        return
+    data = {}
     for name, model in models.items():
-        st.write(f"In Parlament Generations {name} the word '{word}' has a orientation of {word_gender_orientation(word, model)}")
-    
+        data[f"P{name}"] = word_gender_orientation(word, model)
+    h_df = pd.DataFrame(data, index=[word])
+    st.session_state['df'] = pd.concat([h_df, st.session_state['df']])
 
 
 st.title("Embedding Space Analysis")
+st.write("This dashboard provides insights on the projection of specific words in the embedding spaces from the four parlament generations.")
+st.write("The projection is calculated as the normalized dot product between the word and the difference between the mean vectors of v1(herr, kollege, mann) and v2(frau, kollegin, dame).")
 st.write("value < 1: bias towards female")
 st.write("value > 1: bias towards male")
 
@@ -30,5 +40,8 @@ st.write("value > 1: bias towards male")
 word = st.text_input("Enter word to analyze...")
 if word:
     word_gender_over_time(word)
+
+
+st.dataframe(st.session_state['df'].style.highlight_between(left=0.25, right=1, color="lightblue").highlight_between(left=-1, right=-0.25, color="pink"))
 
 
